282bde4bbc8651cfb317e259f610c397852b0bac
Anarkho <alex.soares1414@gmail.com>
2021-07-29 03:44:27 -0300

    configuração inicial para funcionamendo do redux

--- Acai-web/src/index.js
+++ Acai-web/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk' // da a possibilidade dispachar quantos ações forem necessaria 
+import mainReducer from './store';
+
+const store = applyMiddleware(thunk)(createStore)(mainReducer) //store conjunto de reducer, mainReducer o objeto que ficara as reducer 
+
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ Acai-web/src/store/index.js
@@ -0,0 +1,9 @@
+//onde sera armazenada a combinação de todos os reducers
+
+import { combineReducers } from 'redux'
+
+const mainReducer = combineReducers({ //unindo as reducers em um som objeto
+
+})
+
+export default mainReducer
